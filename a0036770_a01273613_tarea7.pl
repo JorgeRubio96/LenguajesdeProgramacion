4afa684d7abeea0c8dbe90ef8fec0d0146c29a21
jorgerubio96 <jorge.rubiobarboza96@gmail.com>
2017-11-22 00:32:44 -0600

    inicio tarea 7 prolog

---
+++ a0036770_a01273613_tarea7.pl
@@ -0,0 +1,39 @@
+% A00368770
+% A01273613
+
+%1.- Tortugas ninja
+% funciones que modelan los enunciados dados
+%respetando el orden de las clausulas
+
+
+%2.- serie
+% funcion que suma los factoriales de un numero dado
+% empezando por el factorial de 1
+
+
+%3.- ultimo
+% funcion que regresa el ultimo elemento de una lista
+
+%4.- salta
+% funcion que a partir de una lista plana genera otra lista
+% con los elementos que se visiten saltando de N en N posiciones
+% a partir del primer elemento
+
+
+%5.- consecutivos
+% funcion que recibe dos elemento en una lista y regresa si son
+% consecutivos o no
+
+%6.- siguiente
+% funcion que regresa el elemento que aparece despues 
+% de otro en una lista
+
+%7.- duplicados
+% funcion que recibe una lista y regresa el numero de atomos duplicados
+
+
+%8.- mezcla
+% funcion que intercala dos listas pre-ordenadas
+%
+
+%9.-
